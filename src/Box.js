3022554b76960f52627ba63d3d2a2a8cc93bd7d5
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-07-15 14:25:33 -0400

    Added button to remove box

--- src/Box.js
+++ src/Box.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
-
+import "./Box.css";
 export default class Box extends Component {
+  handleClick = () => {
+    this.props.removeBox(this.props.id);
+  };
+
   render() {
     const { backgroundColor, height, width } = this.props.boxInfo;
 
     return (
-      <div
-        style={{
-          backgroundColor: backgroundColor,
-          height: `${height}px`,
-          width: `${width}px`,
-        }}
-      />
+      <div className="Box">
+        <div
+          style={{
+            backgroundColor: backgroundColor,
+            height: `${height}px`,
+            width: `${width}px`,
+          }}
+        />
+        <button onClick={this.handleClick}>x</button>
+      </div>
     );
   }
 }
